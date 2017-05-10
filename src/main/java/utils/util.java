069b3c6ef8f1ffda5070a8ac8cde1d17504c849b
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-05-10 11:50:08 +0200

    Added a getToday method for utility

---
+++ src/main/java/utils/util.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+/**
+ * Created by Niki on 2017-05-10.
+ *
+ * @author Niki
+ */
+public class util {
+    public static String getToday() {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        Calendar calendar = new GregorianCalendar();
+        df.setTimeZone(tz);
+        return df.format(calendar.getTime());
+    }
+}
