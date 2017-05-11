2f5447c2c5f0009f805956c006917f02ff286cfb
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-05-11 13:25:48 +0200

    made it follow jax-rs spec more and support the unified urls

--- src/main/java/rest/Flights.java
+++ src/main/java/rest/Flights.java
@@ -2,6 +2,7 @@ package rest;
 
 import com.google.gson.Gson;
 import entity.Airline;
+import utils.util;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.ws.rs.GET;
@@ -9,7 +10,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,18 +29,9 @@ public class Flights {
 
     private static Gson gson = new Gson();
 
-    public static List<String> urls = allUrls();
+    private static List<String> urls = new ArrayList<>(util.urls().values());
 
-    private static List<String> allUrls() {
-        List<String> list = new ArrayList<>();
-        list.add("https://airline.skaarup.io/api/");
-        //list.add("https://airline-plaul.rhcloud.com/api/flightinfo/");
-        list.add("https://vetterlain.dk/AirWonDo/api/");
-
-        return list;
-    }
-
-    public static final Function<String, Airline> c = (String airline) -> {
+    private static final Function<String, Airline> c = (String airline) -> {
         HttpsURLConnection conn = null;
         try {
             URL url = new URL(airline);
@@ -67,36 +61,35 @@ public class Flights {
 
     //Test path: CPH/2017-05-02T00:00:00.000Z/2
     @GET
-    @Path("/{from}/{date}/{tickets}")
+    @Path("{from}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getFlightsFromDateTickets(@PathParam("from") String from,
-            @PathParam("date") String date,
-            @PathParam("tickets") int tickets) {
+                                            @PathParam("date") String date,
+                                            @PathParam("tickets") int tickets) {
         List<Airline> airlines;
         airlines = urls.parallelStream()
-                .map(string -> c.apply(string + "flights/" + from
-                        + "/" + date
-                        + "/" + tickets))
-                .collect(Collectors.toList());
+                       .map(string -> c.apply(string + "flights/" + from
+                                                      + "/" + date
+                                                      + "/" + tickets))
+                       .collect(Collectors.toList());
         return gson.toJson(airlines);
     }
 
     @GET
-    @Path("/{from}/{to}/{date}/{tickets}")
+    @Path("{from}/{to}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getFlightsFromDateTickets(@PathParam("from") String from,
-            @PathParam("to") String to,
-            @PathParam("date") String date,
-            @PathParam("tickets") int tickets) {
+                                            @PathParam("to") String to,
+                                            @PathParam("date") String date,
+                                            @PathParam("tickets") int tickets) {
         List<Airline> airlines;
         airlines = urls.parallelStream()
-                .map(string -> c.apply(string + "flights/" + from
-                        + "/" + to
-                        + "/" + date
-                        + "/" + tickets))
-                .collect(Collectors.toList());
+                       .map(string -> c.apply(string + "flights/" + from
+                                                      + "/" + to
+                                                      + "/" + date
+                                                      + "/" + tickets))
+                       .collect(Collectors.toList());
         return gson.toJson(airlines);
     }
 
-    
 }
